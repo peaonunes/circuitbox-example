b2f6871673ceb00ef249e8de5c571edd2eb2eb8f
Rafael Nunes <rafaelngds@gmail.com>
2020-06-22 20:32:09 +1000

    chore(target-server): add success parameter

--- target-server/index.js
+++ target-server/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 
 app.listen(4000, function () {
- console.log("App listening on port 4000!");
+  console.log("App listening on port 4000!");
 });
 
 app.get("/", function (req, res) {
- setTimeout(function(){
-    res.sendStatus(200);
- }, 500);
+  const success = Boolean(req.query && req.query.success);
+  setTimeout(() => {
+    let status = success === true ? 200 : 400;
+    res.sendStatus(status);
+  }, 250);
 });
